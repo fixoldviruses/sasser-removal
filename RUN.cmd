b452070704fd0b6d7f71b6c6f09ea123da6dc432
Lucas Gabriel (lucmsilva) <90426410+lucmsilva651@users.noreply.github.com>
2023-08-02 16:36:16 -0300

    Added removal script

---
+++ RUN.cmd
@@ -0,0 +1,23 @@
+shutdown -a
+REG DELETE "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "avserve2.exe" /f
+shutdown -a
+taskkill /f /im avserv2.exe
+shutdown -a
+cd c:\
+shutdown -a
+del win2.log
+shutdown -a
+cd %windir%
+shutdown -a
+del avserv2.exe
+shutdown -a
+cls
+echo This tool is made by Lucas Gabriel (lucmsilva)
+echo in 2 August 2023
+echo.
+echo Sassier removed.
+echo Remember to install all updates available.
+echo Just press enter to close this script and restart your computer.
+pause >nul
+shutdown /r /f /t 0
+exit
